c5779954ac3ef075daf2a85c0abafef8cdc3fc34
HALCYON6297 <75776631+HALCYON6297@users.noreply.github.com>
2020-12-29 15:52:14 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -55,7 +55,7 @@ async def amireallyalive(alive):
     cat_caption += f"**  -Telethon version :** `{version.__version__}\n`"
     cat_caption += f"**  -Catuserbot Version :** `{catversion}`\n"
     cat_caption += f"**  -Python Version :** `{python_version()}\n`"
-    cat_caption += f"**  -Master:** {mention}\n"
+    cat_caption += f"**  -Master:** mimi\n"
     results = await bot.inline_query(tgbotusername, cat_caption)  # pylint:disable=E0602
     await results[0].click(alive.chat_id, reply_to=reply_to_id, hide_via=True)
     await alive.delete()
